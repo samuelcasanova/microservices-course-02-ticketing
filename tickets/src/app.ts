1524d8fb43c8bdb223138aed259823ccb5461593
samuel.casanova <samuel.casanova@immfly.com>
2024-07-19 17:46:00 +0200

    improve error logging and add create and show ticket endpoints

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,8 +3,9 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { createTicketRouter } from './routes/new';
 import { errorHandler, NotFoundError, currentUser } from '@samuelcasanovasoft/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,6 +19,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
