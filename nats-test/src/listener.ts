00a29285c8b9993a1608f948c62a6342f5304fa0
samuel.casanova <samuel.casanova@immfly.com>
2024-07-23 08:09:03 +0200

    configure NATS to re-deliver pending/lost messages with durable subscriptions and queue groups

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,23 +1,42 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'queue-group-name',
+    options
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
 
-    if (typeof data !== 'string') {
-      return;
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
 
-    console.log(`Message recieved with sequence ${msg.getSequence()} and data: ${data}`);
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
