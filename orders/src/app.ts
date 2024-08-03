aefad82a10f3e6950d2ce00f958935cd8db57a47
samuel.casanova <samuel.casanova@immfly.com>
2024-08-03 09:30:17 +0200

    add concurrency control over ticket and orders creation/update

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@samuelcasanovasoft/common';
+import { deleteOrderRouter } from './routes/delete';
 import { indexOrderRouter } from './routes/index';
 import { newOrderRouter } from './routes/new';
 import { showOrderRouter } from './routes/show';
-import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,10 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
+app.use(deleteOrderRouter);
 app.use(indexOrderRouter);
 app.use(newOrderRouter);
 app.use(showOrderRouter);
-app.use(deleteOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -66,6 +66,7 @@ router.post(
         id: ticket.id,
         price: ticket.price,
       },
+      version: order.version
     });
 
     res.status(201).send(order);
