84e7a728603e25c0ae6aa6044a4857cf44a8ba97
samuel.casanova <samuel.casanova@immfly.com>
2024-08-13 08:21:51 +0200

    CICD changes to execute tests on every pull_request change, and to dpeloy to Digital Ocean kubernetes cluster on each merge to master

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -37,7 +37,7 @@ const OrderShow = ({ order, currentUser }) => {
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
         token={({ id }) => doRequest({ token: id })}
-        stripeKey="pk_test_JMdyKVvf8EGTB0Fl28GsN7YY"
+        stripeKey="pk_test_51MM7iTCan3TmmXmaBjXZfUcQOC9wWC3fVOwN0z9WYYbZXKOO4R7VMH3sw3LOWmvPYFdj4fAYIKI5BoO7V71EP0Xi00sb0fqhHl"
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
